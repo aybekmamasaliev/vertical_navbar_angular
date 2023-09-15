2c4fa6f115f2e85e6bbf39eb435b1596b4bde36a
aybek <aybek.mamasaliev@bk.ru>
2023-09-15 23:06:37 +0600

    feat: finished side nav

--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -3,5 +3,35 @@ export const navbarData=[
         routerlink:"dashboard", 
         icon:"fa fa-home",
         label:"Dashboard"
-    }
+    },
+    {
+        routerlink:"products", 
+        icon:"fa fa-th-large",
+        label:"Products"
+    },
+    {
+        routerlink:"statistics", 
+        icon:"fa fa-stack-overflow",
+        label:"Statistics"
+    },
+    {
+        routerlink:"coupens", 
+        icon:"fa fa-sort-amount-asc",
+        label:"Coupens"
+    },
+    {
+        routerlink:"pages", 
+        icon:"fa fa-sign-language",
+        label:"Pages"
+    },
+    {
+        routerlink:"media", 
+        icon:"fa fa-shield",
+        label:"Media"
+    },
+    {
+        routerlink:"settings", 
+        icon:"fa fa-sun-o",
+        label:"Settings"
+    },
 ]
